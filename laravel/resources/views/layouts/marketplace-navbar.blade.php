63661c3e29de26ab3a1406ebe89a8d1c266795be
Kharisma14 <kharismanisa14@gmail.com>
2023-07-17 20:35:37 +0700

    perbaikan error

--- laravel/resources/views/layouts/marketplace-navbar.blade.php
+++ laravel/resources/views/layouts/marketplace-navbar.blade.php
@@ -19,6 +19,7 @@
             <button data-modal-target="jadianggota-modal" data-modal-toggle="jadianggota-modal" type="button">
             Daftar jadi Anggota
             </button>
+        else
         @endif
     </div>
     <div class="flex md:flex-wrap items-center justify-around md:justify-between md:mx-auto py-2 px-2 md:px-12">
@@ -86,6 +87,7 @@
     </div>
   </nav>
 
+@if(!Auth::guest() && Auth::user()->role == 'pembeli')
 <div id="jadianggota-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative w-full max-w-md max-h-full">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -108,3 +110,4 @@
         </div>
     </div>
 </div>
+@endif
